271c8fe3c43d0f51316fa6f95421426c2e4c9450
Baarti-Baerti <martin.baart@gmail.com>
2026-02-23 21:18:32 +0100

    Add files via upload

--- garmin-backend/garmin/__init__.py
+++ garmin-backend/garmin/__init__.py
@@ -1,1 +1,33 @@
-# api package
+from .session import get_client, login_and_save, is_authenticated
+from .fetcher import (
+    fetch_daily_summaries,
+    fetch_activities_last_n_days,
+    fetch_activities_for_month,
+    fetch_latest_bmi,
+)
+from .transform import build_user_payload
+from .registry import (
+    all_members,
+    get_member,
+    get_by_google_sub,
+    add_member,
+    update_member,
+    remove_member,
+)
+
+__all__ = [
+    "get_client",
+    "login_and_save",
+    "is_authenticated",
+    "fetch_daily_summaries",
+    "fetch_activities_last_n_days",
+    "fetch_activities_for_month",
+    "fetch_latest_bmi",
+    "build_user_payload",
+    "all_members",
+    "get_member",
+    "get_by_google_sub",
+    "add_member",
+    "update_member",
+    "remove_member",
+]
